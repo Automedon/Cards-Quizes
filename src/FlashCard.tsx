63a9d93855d95ccfdfb3c2a4bc168a7d4617d10c
Automedon <42299107+Automedon@users.noreply.github.com>
2020-09-19 17:47:53 +0700

    card styling

--- src/FlashCard.tsx
+++ src/FlashCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface FlashCardProps {
   flashcard: {
@@ -10,7 +10,21 @@ interface FlashCardProps {
 }
 
 const FlashCard: React.FC<FlashCardProps> = ({ flashcard }) => {
-  return <div>{flashcard.question}</div>;
+  const [flip, setFlip] = useState(false);
+  return (
+    <div className={`card ${flip && "flip"}`} onClick={() => setFlip(!flip)}>
+      <div className="front">
+        {flashcard.question}
+        <div className="flashcard-options">
+          {flashcard.options.map((option) => {
+            return <div className="flashcard-option">{option}</div>;
+          })}
+        </div>
+      </div>
+      <div className="back">{flashcard.answer}</div>
+      {flip ? flashcard.answer : flashcard.question}
+    </div>
+  );
 };
 
 export default FlashCard;
